1dd7262abfd6ca52d29faefe249c51b06c498e7a
StevensonJ0047 <StevensonJ0047@sduhsd.lan>
2017-11-29 11:52:22 -0800

    Completed 1_BasicPrinting-Assignment

---
+++ ECS_Java/src/GoodFirstProgram.java
@@ -0,0 +1,19 @@
+
+public class GoodFirstProgram {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	//	System.out.println("Hello again");
+		
+		System.out.println("I like typing this");
+		System.out.println("This s fun");
+		
+		System.out.println("Yay! Printing");
+		System.out.println("I'd much rather you 'not'");
+		System.out.println("I \"said\" do not touch this");
+		
+		//A:
+		System.out.println("Hello, once more!");
+		
+		//B: It turns the line into a comment, where it does not affect the code.
+	}
+}
---
+++ ECS_Java/src/YourInitials.java
@@ -0,0 +1,36 @@
+
+public class YourInitials {
+	public static void main(String[] args) {
+		
+		//The J Character
+		System.out.println("J J J J J");
+		System.out.println("    J    ");
+		System.out.println("    J    ");
+		System.out.println("    J    ");
+		System.out.println("J   J    ");
+		System.out.println("J   J    ");
+		System.out.println("J J J    ");
+		
+		System.out.println("");
+		
+		//The P Character
+		System.out.println("P P P P  ");
+		System.out.println("P       P");
+		System.out.println("P       P");
+		System.out.println("P P P P  ");
+		System.out.println("P        ");
+		System.out.println("P        ");
+		System.out.println("P        ");
+		
+		System.out.println("");
+		
+		//The S Character
+		System.out.println("  S S S  ");
+		System.out.println("S       S");
+		System.out.println("S        ");
+		System.out.println("  S S S  ");
+		System.out.println("        S");
+		System.out.println("S       S");
+		System.out.println("  S S S  ");
+	}
+}
